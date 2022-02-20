683f0ac15f20f5dde18b35e58d7c6a91ac953b5b
Muhammad Avtara Khrisna <avtarakhrisna1@gmail.com>
2022-02-20 12:51:34 +0700

    store id activity

--- main.rb
+++ main.rb
@@ -1,4 +1,4 @@
 require_relative  'report'
 
-reports = Report.new("johndoe@gmail.com", "xxxx")
-reports.getIDActivity
+reports = Report.new("avtarakhrisna1@gmail.com", "JokoSur123!")
+reports.get_id_activity
--- report.rb
+++ report.rb
@@ -1,9 +1,11 @@
+#!/usr/bin/ruby
 require 'faraday'
 
 class Report
-  attr_reader :token
+  attr_reader :token, :id_activity
 
   def initialize(email, password)
+    @id_activity = []
     @conn = Faraday.new(
       url: 'https://api.kampusmerdeka.kemdikbud.go.id'
     )
@@ -15,16 +17,19 @@ class Report
     end
 
     response.status == 200 ? @token = JSON.parse(response.body)["data"]["access_token"]
-      : puts(JSON.parse(response.body)["error"]["message"])
+      : abort(JSON.parse(response.body)["error"]["message"])
   end
 
-  def getIDActivity
+  def get_id_activity
     response = @conn.get('/mbkm/mahasiswa/active-activities/my') do |req|
       req.headers['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'
       req.headers['Authorization'] = "Bearer #{@token}"
     end
 
-    data = JSON.parse(response.body)["data"]
-    data.each_with_index { |val, index| puts "#{index + 1}. #{val["nama_kegiatan"]} by #{val["mitra_brand_name"]} => #{val["id"]}" }
+    datas = JSON.parse(response.body)["data"]
+    datas.each_with_index do |val, index|
+      puts "#{index + 1}. #{val["nama_kegiatan"]} by #{val["mitra_brand_name"]} => #{val["id"]}"
+      @id_activity.push(val["id"])
+    end
   end
 end
